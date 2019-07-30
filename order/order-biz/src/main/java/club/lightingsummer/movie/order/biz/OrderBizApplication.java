4b9cc40e36814d2dd4464de5f41da1fb7175a7d4
lightingSummer <1356221057@qq.com>
2019-07-30 10:30:38 +0800

    framework

---
+++ order/order-biz/src/main/java/club/lightingsummer/movie/order/biz/OrderBizApplication.java
@@ -0,0 +1,13 @@
+package club.lightingsummer.movie.order.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderBizApplication.class, args);
+    }
+
+}
